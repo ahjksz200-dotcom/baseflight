e656340692ab262fb08c9efc3dbdf4b8ec394fb7
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-03-27 14:49:22 +0700

    Update board.h

--- src/board.h
+++ src/board.h
@@ -30,94 +30,44 @@
 #define M_PI       3.14159265358979323846f
 #endif
 
-#define RADX10 (M_PI / 1800.0f)
-#define RAD    (M_PI / 180.0f)
-
-#define min(a, b) ((a) < (b) ? (a) : (b))
-#define max(a, b) ((a) > (b) ? (a) : (b))
-#define abs(x) ((x) > 0 ? (x) : -(x))
-
-#define U_ID_0 (*(uint32_t*)0x1FFFF7E8)
-#define U_ID_1 (*(uint32_t*)0x1FFFF7EC)
-#define U_ID_2 (*(uint32_t*)0x1FFFF7F0)
-
-// --- 1. ENUMS & TYPEDEFS (GIỮ NGUYÊN ĐỂ KHÔNG LỖI BIÊN DỊCH) ---
-
-typedef enum HardwareRevision {
-    NAZE32 = 1,
-    NAZE32_REV5,
-    NAZE32_SP,
-    NAZE32_REV6,
-} HardwareRevision;
-
+// --- 1. CORE TYPES & ENUMS ---
+typedef enum { X = 0, Y, Z } sensor_axis_e;
 typedef enum {
-    X = 0, Y, Z
-} sensor_axis_e;
-
-typedef enum {
-    ALIGN_DEFAULT = 0,
-    CW0_DEG, CW90_DEG, CW180_DEG, CW270_DEG,
+    ALIGN_DEFAULT = 0, CW0_DEG, CW90_DEG, CW180_DEG, CW270_DEG,
     CW0_DEG_FLIP, CW90_DEG_FLIP, CW180_DEG_FLIP, CW270_DEG_FLIP
 } sensor_align_e;
 
+typedef enum HardwareRevision { NAZE32 = 1, NAZE32_REV5, NAZE32_SP, NAZE32_REV6 } HardwareRevision;
+
 typedef enum {
-    SENSOR_GYRO = 1 << 0,
-    SENSOR_ACC = 1 << 1,
-    SENSOR_BARO = 1 << 2,
-    SENSOR_MAG = 1 << 3,
-    SENSOR_SONAR = 1 << 4,
-    SENSOR_GPS = 1 << 5,
-    SENSOR_GPSMAG = 1 << 6,
+    SENSOR_GYRO = 1 << 0, SENSOR_ACC = 1 << 1, SENSOR_BARO = 1 << 2,
+    SENSOR_MAG = 1 << 3, SENSOR_SONAR = 1 << 4, SENSOR_GPS = 1 << 5, SENSOR_GPSMAG = 1 << 6,
 } AvailableSensors;
 
-typedef enum AccelSensors {
-    ACC_DEFAULT = 0, ACC_ADXL345, ACC_MPU6050, ACC_MMA8452, ACC_BMA280, ACC_MPU6500, ACC_NONE
-} AccelSensors;
-
-typedef enum CompassSensors {
-    MAG_DEFAULT = 0, MAG_HMC5883L, MAG_AK8975, MAG_NONE
-} CompassSensors;
-
 typedef enum {
-    FEATURE_PPM = 1 << 0,
-    FEATURE_VBAT = 1 << 1,
-    FEATURE_INFLIGHT_ACC_CAL = 1 << 2,
-    FEATURE_SERIALRX = 1 << 3,
-    FEATURE_MOTOR_STOP = 1 << 4,
-    FEATURE_SERVO_TILT = 1 << 5,
-    FEATURE_SOFTSERIAL = 1 << 6,
-    FEATURE_LED_RING = 1 << 7,
-    FEATURE_GPS = 1 << 8,
-    FEATURE_FAILSAFE = 1 << 9,
-    FEATURE_SONAR = 1 << 10,
-    FEATURE_TELEMETRY = 1 << 11,
-    FEATURE_POWERMETER = 1 << 12,
-    FEATURE_VARIO = 1 << 13,
-    FEATURE_3D = 1 << 14,
-    FEATURE_FW_FAILSAFE_RTH = 1 << 15,
-    FEATURE_SYNCPWM = 1 << 16,
-    FEATURE_FASTPWM = 1 << 17,
+    FEATURE_PPM = 1 << 0, FEATURE_VBAT = 1 << 1, FEATURE_INFLIGHT_ACC_CAL = 1 << 2,
+    FEATURE_SERIALRX = 1 << 3, FEATURE_MOTOR_STOP = 1 << 4, FEATURE_SERVO_TILT = 1 << 5,
+    FEATURE_SOFTSERIAL = 1 << 6, FEATURE_LED_RING = 1 << 7, FEATURE_GPS = 1 << 8,
+    FEATURE_FAILSAFE = 1 << 9, FEATURE_SONAR = 1 << 10, FEATURE_TELEMETRY = 1 << 11,
+    FEATURE_POWERMETER = 1 << 12, FEATURE_VARIO = 1 << 13, FEATURE_3D = 1 << 14,
 } AvailableFeatures;
 
-typedef enum {
-    SERIALRX_SPEKTRUM1024 = 0, SERIALRX_SPEKTRUM2048, SERIALRX_SBUS, SERIALRX_SUMD, SERIALRX_MSP, SERIALRX_IBUS, SERIALRX_PROVIDER_MAX = SERIALRX_IBUS
-} SerialRXType;
+// Telemetry & Serial
+typedef enum { TELEMETRY_PROVIDER_FRSKY = 0, TELEMETRY_PROVIDER_HOTT } TelemetryProvider;
+typedef enum { TELEMETRY_PORT_UART = 0, TELEMETRY_PORT_SOFTSERIAL_1, TELEMETRY_PORT_SOFTSERIAL_2 } TelemetryPort;
+typedef enum { SERIAL_BAUD_9600 = 0, SERIAL_BAUD_19200, SERIAL_BAUD_38400, SERIAL_BAUD_57600, SERIAL_BAUD_115200 } SerialBaudRate;
 
-typedef enum {
-    GPS_NMEA = 0, GPS_UBLOX, GPS_MTK_NMEA, GPS_MTK_BINARY, GPS_MAG_BINARY, GPS_HARDWARE_MAX = GPS_MAG_BINARY
-} GPSHardware;
+// GPS
+typedef enum { GPS_NMEA = 0, GPS_UBLOX, GPS_MTK_NMEA, GPS_MTK_BINARY, GPS_MAG_BINARY } GPSHardware;
+typedef enum { GPS_BAUD_115200 = 0, GPS_BAUD_57600, GPS_BAUD_38400, GPS_BAUD_19200, GPS_BAUD_9600, GPS_BAUD_MAX = GPS_BAUD_9600 } GPSBaudRates;
 
-typedef enum {
-    GPS_BAUD_115200 = 0, GPS_BAUD_57600, GPS_BAUD_38400, GPS_BAUD_19200, GPS_BAUD_9600, GPS_BAUD_MAX = GPS_BAUD_9600
-} GPSBaudRates;
-
+// --- 2. FUNCTION POINTERS & SENSOR STRUCTS ---
 typedef void (*sensorInitFuncPtr)(sensor_align_e align);
 typedef void (*sensorReadFuncPtr)(int16_t *data);
 typedef void (*baroOpFuncPtr)(void);
 typedef void (*baroCalculateFuncPtr)(int32_t *pressure, int32_t *temperature);
 typedef void (*serialReceiveCallbackPtr)(uint16_t data);
 typedef uint16_t (*rcReadRawDataPtr)(uint8_t chan);
-typedef void (*pidControllerFuncPtr)(void);
 
 typedef struct sensor_t {
     sensorInitFuncPtr init;
@@ -127,28 +77,24 @@ typedef struct sensor_t {
 } sensor_t;
 
 typedef struct baro_t {
-    uint16_t ut_delay;
-    uint16_t up_delay;
-    baroOpFuncPtr start_ut;
-    baroOpFuncPtr get_ut;
-    baroOpFuncPtr start_up;
-    baroOpFuncPtr get_up;
+    uint16_t ut_delay; uint16_t up_delay;
+    baroOpFuncPtr start_ut; baroOpFuncPtr get_ut;
+    baroOpFuncPtr start_up; baroOpFuncPtr get_up;
     baroCalculateFuncPtr calculate;
 } baro_t;
 
-// --- 2. PINOUT CHO BLUE PILL (THAY ĐỔI TẠI ĐÂY) ---
+// --- 3. BLUE PILL HARDWARE MAPPING ---
 #if defined(NAZE)
-
 #define LED0_GPIO   GPIOC
-#define LED0_PIN    Pin_13      // LED Blue Pill (PC13)
+#define LED0_PIN    Pin_13      // LED duy nhất trên Blue Pill
 #define LED1_GPIO   GPIOC 
-#define LED1_PIN    Pin_13      // Dùng chung PC13 để fix lỗi Driver
+#define LED1_PIN    Pin_13 
 
 #define BEEP_GPIO   GPIOA
-#define BEEP_PIN    Pin_15      // Dời khỏi PA12 để giải phóng chân USB
+#define BEEP_PIN    Pin_15      // Tránh chân USB PA11/PA12
 
 #define BARO_GPIO   GPIOB
-#define BARO_PIN    Pin_2       // Tránh PC13
+#define BARO_PIN    Pin_2 
 
 #define GYRO
 #define ACC
@@ -163,9 +109,9 @@ typedef struct baro_t {
 #define MOTOR_PWM_RATE 400
 
 #define SENSORS_SET (SENSOR_ACC | SENSOR_BARO | SENSOR_MAG)
-#define I2C_DEVICE (I2CDEV_2)   // Dùng I2C2 (PB10, PB11) cho MPU6500
+#define I2C_DEVICE (I2CDEV_2)   // Sử dụng PB10 (SCL) và PB11 (SDA)
 
-// --- 3. INCLUDES (PHẢI Ở DƯỚI TYPEDEF) ---
+// --- 4. DRIVER INCLUDES ---
 #include "drv_adc.h"
 #include "drv_adxl345.h"
 #include "drv_bma280.h"
@@ -191,19 +137,14 @@ typedef struct baro_t {
 #error TARGET NOT DEFINED!
 #endif
 
-// --- 4. MACROS ---
-#ifdef LED0
-#define LED0_TOGGLE              digitalToggle(LED0_GPIO, LED0_PIN);
-#define LED0_OFF                 digitalHi(LED0_GPIO, LED0_PIN);
-#define LED0_ON                  digitalLo(LED0_GPIO, LED0_PIN);
-#endif
-#ifdef LED1
-#define LED1_TOGGLE              digitalToggle(LED1_GPIO, LED1_PIN);
-#define LED1_OFF                 digitalHi(LED1_GPIO, LED1_PIN);
-#define LED1_ON                  digitalLo(LED1_GPIO, LED1_PIN);
-#endif
-#ifdef BEEP_GPIO
-#define BEEP_TOGGLE              digitalToggle(BEEP_GPIO, BEEP_PIN);
-#define BEEP_OFF                 systemBeep(false);
-#define BEEP_ON                  systemBeep(true);
-#endif
+// --- 5. SYSTEM MACROS ---
+#define LED0_TOGGLE digitalToggle(LED0_GPIO, LED0_PIN);
+#define LED0_OFF    digitalHi(LED0_GPIO, LED0_PIN);
+#define LED0_ON     digitalLo(LED0_GPIO, LED0_PIN);
+#define LED1_TOGGLE digitalToggle(LED1_GPIO, LED1_PIN);
+#define LED1_OFF    digitalHi(LED1_GPIO, LED1_PIN);
+#define LED1_ON     digitalLo(LED1_GPIO, LED1_PIN);
+
+#define BEEP_TOGGLE digitalToggle(BEEP_GPIO, BEEP_PIN);
+#define BEEP_OFF    systemBeep(false);
+#define BEEP_ON     systemBeep(true);
